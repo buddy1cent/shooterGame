fc87d6f3403ef134f6cd13eb484dc219df875a70
Buddy-1cent <Buddy-1cent@192.168.0.102>
2014-12-03 21:25:34 +0100

    uz bez novinek

--- src/shootergame/ShooterGame.java
+++ src/shootergame/ShooterGame.java
@@ -8,7 +8,6 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GLContext;
 import org.lwjgl.util.vector.Vector3f;
 import org.newdawn.slick.opengl.PNGDecoder;
 
